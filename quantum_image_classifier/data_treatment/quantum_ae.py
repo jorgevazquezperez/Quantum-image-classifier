27d4789dccb3798e9e086e562f70b3df74f14b3b
Jorge Vázquez Pérez <jorge.vazper@gmail.com>
2022-05-26 18:50:26 +0200

    Autoencoder simulation implemented without train.

--- quantum_image_classifier/data_treatment/quantum_ae.py
+++ quantum_image_classifier/data_treatment/quantum_ae.py
@@ -1,20 +1,25 @@
+import numpy as np
+
 from qiskit.circuit import ParameterVector
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer
+
+from ..encoding import PhaseEncoding
 
 class QuantumAE():
     def __init__(self, num_qubits: int, n_components: int):
         self.num_qubits = num_qubits
-        self._build_autoencoder(self.num_qubits - n_components)
+        self.n_trash = num_qubits - n_components
+        self._build_autoencoder(self.n_trash)
 
-    def _build_autoencoder(self, n_trash: int):
+    def _build_autoencoder(self):
         
         self._build_circuit()
-        self._build_SWAPTest(n_trash)
+        self._build_SWAPTest(self.n_trash)
         
-        qregisters = QuantumRegister(self.num_qubits + n_trash + 1, "q")
-        self.autoencoder = QuantumCircuit(qregisters, name="Autoencoder")
-        self.autoencoder.compose(self.circuit, qubits=qregisters[n_trash + 1:], inplace = True)
-        self.autoencoder.compose(self.swap_test, qubits=qregisters[:2*n_trash + 1], inplace=True)
+        self.qregisters = QuantumRegister(self.num_qubits + self.n_trash + 1, "q")
+        self.autoencoder = QuantumCircuit(self.qregisters, name="Autoencoder")
+        self.autoencoder.compose(self.circuit, qubits=self.qregisters[self.n_trash + 1:], inplace = True)
+        self.autoencoder.compose(self.swap_test, qubits=self.qregisters[:2*self.n_trash + 1], inplace=True)
 
         print(self.autoencoder)
 
@@ -56,6 +61,23 @@ class QuantumAE():
             self.swap_test.cswap(qregisters[0], qregisters[i+1], qregisters[n_trash+i+1])
         self.swap_test.h(qregisters[0])
 
-    def fit():
-        pass
+    def fit(self, dataset: np.ndarray = None) -> None:
+        
+        for input_vector in dataset:
+            state = PhaseEncoding(input_vector)
 
+            cregister = ClassicalRegister(1, "c")
+            circ_aux = QuantumCircuit(self.qregisters, cregister)
+            circ_aux.compose(state.circuit, qubits=self.qregisters[self.n_trash + 1:], inplace=True)
+            circ_aux.compose(self.autoencoder, qubits=self.qregisters[self.n_trash + 1:], inplace=True)
+
+            circ_aux.measure(self.qregisters[0], cregister[0])
+            simulator = Aer.get_backend('aer_simulator')
+            result = simulator.run(circ_aux, shots=1000).result()
+            counts = result.get_counts(circ_aux)
+
+            if '1' in counts.keys():
+                z = counts['1'] / 1000
+            else:
+                z = 0
+            return np.sqrt(z)
